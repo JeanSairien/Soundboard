8ad869bf172c9a20e89ad6e20d742ad499c94a6e
Fry <fry@Fry.home>
2019-11-21 15:08:35 +0100

    [ADD] russie

--- js/main.js
+++ js/main.js
@@ -10,10 +10,10 @@ $(document).ready(function() {
         o(a), a.paused ? a.play() : (a.pause(), a.currentTime = 0)
     })
     
-	// var cards = $(".item");
-	// for(var i = 0; i < cards.length; i++){
-	//     var target = Math.floor(Math.random() * cards.length -1) + 1;
-	//     var target2 = Math.floor(Math.random() * cards.length -1) +1;
-	//     cards.eq(target).before(cards.eq(target2));
-	// }
+	var cards = $(".item");
+	for(var i = 0; i < cards.length; i++){
+	    var target = Math.floor(Math.random() * cards.length -1) + 1;
+	    var target2 = Math.floor(Math.random() * cards.length -1) +1;
+	    cards.eq(target).before(cards.eq(target2));
+	}
 });
